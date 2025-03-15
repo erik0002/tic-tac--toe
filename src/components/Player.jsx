0612a45067520b21352b75df6ce2dbc49297b938
Yeliazar <erik.ruzhakov@mail.ru>
2025-03-15 23:38:34 +0300

    Add: condition by button isEditing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,13 +5,13 @@ export default function Player({name, symbol}) {
     const [ isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
 
     let playerName = <span className="player-name">{name}</span>
 
     if (isEditing) {
-        playerName = <input type="text" required/>
+        playerName = <input type="text" required value={name} />
     }
 
     return (<li>
@@ -19,6 +19,6 @@ export default function Player({name, symbol}) {
               {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Save</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit" }</button>
           </li>)
 }
